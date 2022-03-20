1909fc5ee43ad2231f50a25289087f3d0fce9608
GulchinTaghizade <1pylr6h@code.edu.az>
2022-03-21 03:07:44 +0400

    uploaded homework

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace FirstProject
 {
@@ -6,45 +7,172 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            MaxNumber(-120, 151, 45, 89, 0);
-            NumOfOnes(7);
+            //int[] massiv = { 3, 5, 7 };
+            //int number = 9;
+            //AddNumber(ref massiv, number);
+            //foreach (var item in massiv)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //string name = "Hikmet Aliyev";
+            //Console.WriteLine(SpaceRemover(ref name));
+            //Console.WriteLine(name);
+
+
+            //int num = 34;
+            //Console.WriteLine(NearestSqrt(ref num));
+            //Console.WriteLine(num);
+        }
+
+        #region C# methods
+        //static int MaxNumber(params int[] arr) //Task1
+        //{
+        //    int max = arr[0];
+        //    foreach (var item in arr)
+        //    {
+        //        if (item > max)
+        //        {
+        //            max = item;
+
+        //        }
+        //    }
+        //    return max;
+
+        //}
+
+        //static int NumOfOnes(int num) //Task2
+        //{
+        //    string Result = "";
+        //    for (int i = 0; num > 0; i++)
+        //    {
+        //        Result = num % 2 + Result;
+        //        num = num / 2;
+        //    }
+        //    int count = 0;
+        //    for (int i = 0; i < Result.Length; i++)
+        //    {
+        //        if (Result[i]%2 == 1)
+        //        {
+        //            count++;
+        //        }
+
+        //    }
+        //    return count;
+
+        //}
+        #endregion
+
+        #region String. StringBuilder
+        static int indexOf(char value)     //Task1.1
+        {
+            string text = "salamlar";
+            int result = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == value)
+                {
+                    return i;
+
+                }
+            }
+            return result;
+        }
+
+        static int indexOf(char value, int index)     //Task1.2
+        {
+            string text = "salamlar";
+            int result = 0;
+            for (int i = index; i < text.Length; i++)
+            {
+                if (text[i] == value)
+                {
+                    return i;
+
+                }
+            }
+            return result;
         }
 
 
-        static int MaxNumber(params int[] arr) //Task1
+        static int indexOf(char value, int index, int count)     //Task1.3
         {
-            int max = arr[0];
+            string text = "salamlar";
+            int result = 0;
+            for (int i = index; i < count; i++)
+            {
+                if (text[i] == value)
+                {
+                    return i;
+
+                }
+            }
+            return result;
+        }
+
+
+
+        static StringBuilder reverse(string value)              //Task2
+        {
+            StringBuilder revValue = new StringBuilder();
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+
+                revValue.Append(value[i]);
+
+            }
+            return revValue;
+        }
+
+        #endregion
+
+        #region C# ref/out
+        // TASK1
+        static void AddNumber(ref int[] arr, int num) 
+        {
+            int[] array = new int[arr.Length + 1];
             foreach (var item in arr)
             {
-                if (item > max)
-                {
-                    max = item;
 
-                }
+                Console.WriteLine(item);
             }
-            Console.WriteLine(max);
-            return max;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                array[i] = arr[i];
+            }
+            array[arr.Length] = num;
+            arr = array;
         }
-        static int NumOfOnes(int num) //Task2
+
+        //TASK2
+        static StringBuilder SpaceRemover(ref string value)
         {
-            string Result = "";
-            for (int i = 0; num > 0; i++)
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < value.Length; i++)
             {
-                Result = num % 2 + Result;
-                num = num / 2;
-            }
-            int count = 0;
-            for (int i = 0; i < Result.Length; i++)
-            {
-                if (Result[i]%2 == 1)
+                if (value[i] != ' ')
                 {
-                    count++;
+                    result.Append(value[i]);
                 }
-
             }
-            Console.WriteLine(count);
-            return count;
+            value = result.ToString();
+            return result;
+
         }
 
+        //TASK3
+        static int NearestSqrt(ref int num)
+        {
+            int result = 1;
+            for (int i = 1; i*i <= num; i++)
+            {
+                result = i;
+            }
+            num = result;
+            return result;
+        }
+
+        #endregion
     }
 }
